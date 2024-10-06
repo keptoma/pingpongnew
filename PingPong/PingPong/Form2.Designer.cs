6709aaf45933c098a1fc2fd89885cdafd4807131
Tamara Hovhannisyan <tamara.hovhannisyan.20@gmail.com>
2024-10-06 23:22:40 +0400

    Add files via upload

---
+++ PingPong/PingPong/Form2.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace PingPong
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.paddle2 = new System.Windows.Forms.PictureBox();
+            this.paddle1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(751, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.BackgroundImage = global::PingPong.Properties.Resources.purepng_com_ping_pong;
+            this.Ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Ball.Location = new System.Drawing.Point(349, 187);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(28, 27);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // paddle2
+            // 
+            this.paddle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.paddle2.Location = new System.Drawing.Point(767, 208);
+            this.paddle2.Name = "paddle2";
+            this.paddle2.Size = new System.Drawing.Size(21, 90);
+            this.paddle2.TabIndex = 1;
+            this.paddle2.TabStop = false;
+            // 
+            // paddle1
+            // 
+            this.paddle1.BackColor = System.Drawing.Color.SteelBlue;
+            this.paddle1.Location = new System.Drawing.Point(12, 124);
+            this.paddle1.Name = "paddle1";
+            this.paddle1.Size = new System.Drawing.Size(22, 90);
+            this.paddle1.TabIndex = 0;
+            this.paddle1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.paddle2);
+            this.Controls.Add(this.paddle1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paddle1;
+        private System.Windows.Forms.PictureBox paddle2;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ PingPong/PingPong/Form2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form2 : Form
+    {
+        int xSpeed = 10;
+        int ySpeed = 10;
+        int player1 = 0;
+        int player2 = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.W)
+            {
+                paddle1.Top -= 5;
+            }
+            if(e.KeyCode==Keys.S)
+            {
+                paddle1.Top += 5;
+            }
+
+            if(e.KeyCode==Keys.A)
+            {
+                paddle2.Top -= 5;
+
+            }
+            if (e.KeyCode == Keys.M)
+            {
+                paddle2.Top += 5;
+
+            }
+
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Ball.Top += ySpeed;
+            Ball.Left += xSpeed;
+
+            if(Ball.Location.Y>=420)
+            {
+                 ySpeed = - ySpeed;
+            }
+
+            if(Ball.Location.X>=760)
+            {
+                xSpeed = -xSpeed;
+                player1++;
+                label1.Text = player1.ToString();
+            }
+
+            if(Ball.Location.X<=0)
+            {
+                xSpeed = -xSpeed;
+                player2++;
+                label2.Text = player2.ToString();
+            }
+           if(Ball.Location.Y<=0)
+           {
+                ySpeed = -ySpeed;
+           }
+
+            if (Ball.Bounds.IntersectsWith(paddle2.Bounds))
+            {
+                xSpeed = -xSpeed;
+            }
+
+            if (Ball.Bounds.IntersectsWith(paddle1.Bounds))
+            {
+                xSpeed = -xSpeed;
+            }
+             
+
+        }
+    }
+}
